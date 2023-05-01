0629b781728f607e71b01196e1439c701fb80f93
willylakshit <lakshitt428@gmail.com>
2023-05-02 00:33:33 +0530

    RMX2020: Blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_RMX2020.mk
+    $(LOCAL_DIR)/bliss_RMX2020.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_RMX2020-user \
-    superior_RMX2020-userdebug \
-    superior_RMX2020-eng
+    bliss_RMX2020-user \
+    bliss_RMX2020-userdebug \
+    bliss_RMX2020-eng
--- bliss_RMX2020.mk
+++ bliss_RMX2020.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common BlissROMs stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_RMX2020
+PRODUCT_NAME := bliss_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2020
@@ -26,9 +26,12 @@ PRODUCT_SYSTEM_MODEL := RMX2020
 PRODUCT_SYSTEM_NAME := RMX2020
 PRODUCT_SYSTEM_DEVICE := RMX2020
 
-# SuperiorOS additions
+# Blissify
 TARGET_BOOT_ANIMATION_RES := 720
-SUPERIOR_OFFICIAL := true
+TARGET_INCLUDE_MATLOG := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+USE_MOTO_CALCULATOR := true
+BLISS_BUILDTYPE := OFFICIAL
 
 # Build info
 BUILD_FINGERPRINT := "realme/RMX2020/RMX2020:10/QP1A.190711.020/1651798546:user/release-keys"
